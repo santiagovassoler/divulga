534df54ee7181a435630a8636ca1846072a1bc86
santiagovassoler <santiagovassoler@gmail.com>
2022-07-25 23:09:04 +0100

    user model

---
+++ priv/repo/migrations/20220722134747_create_users.exs
@@ -0,0 +1,17 @@
+defmodule Divulga.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      add :email, :string, unique: true
+      add :password_hash, :string
+      add :role, :string
+
+      timestamps()
+    end
+     
+    create unique_index(:users, [:email])
+  end
+end
